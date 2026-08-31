d3cb88c8ff4b069aa42f59f416d9827e470dd668
Mathew Muindi <mathewmuindi82@gmail.com>
2026-08-31 20:03:18 +0300

    Initial project setup

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author Cui
+ */
+public class Hello
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello from Advanced Java & Java FX!");
+    }
+    
+}
---
+++ src/com/adse2509/sess01_java_util_apis/UtilHello.java
@@ -0,0 +1,18 @@
+package com.adse2509.sess01_java_util_apis;
+
+/**
+ *
+ * @author Cui
+ */
+public class UtilHello
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello from java.util package demo!");
+    }
+    
+}
